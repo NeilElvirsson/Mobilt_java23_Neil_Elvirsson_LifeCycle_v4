7a3a2897257590474a03bdbcc719fcd4a51a5c5f
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2024-09-04 19:48:19 +0200

    Added method for phone + radiobutton(radiobuttons defect)

--- app/src/main/java/com/example/lifecyclev4/MainActivity2.kt
+++ app/src/main/java/com/example/lifecyclev4/MainActivity2.kt
@@ -7,7 +7,10 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +22,11 @@ class MainActivity2 : AppCompatActivity() {
     val db = Firebase.firestore
     private lateinit var name: String
     private lateinit var lastName: String
+    private lateinit var phone: String
+    private lateinit var radioGrop: RadioGroup
+    private lateinit var selectedRadioButton: RadioButton
+
+
     private val sharedPreFile = "com.example.lifecyclev4.PREFERENCE_FILE_KEY"
 
 
@@ -29,7 +37,6 @@ class MainActivity2 : AppCompatActivity() {
 
         //-------------Top--------------------
 
-
         val homeButton = findViewById<Button>(R.id.homebutton)
 
         homeButton.setOnClickListener {
@@ -37,34 +44,65 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(homeIntent)
         }
 
+
+
+
+
         val sharedPreferences = getSharedPreferences(sharedPreFile, MODE_PRIVATE)
 
         this.name = sharedPreferences.getString("name", "") ?: ""
         this.lastName = sharedPreferences.getString("lastName", "") ?: ""
+        this.phone = sharedPreferences.getString("phone", "") ?:""
+        val savedRadioButtonId = sharedPreferences.getInt("selectedRadioButton", -1)
+
 
         val nameEditText = findViewById<TextView>(R.id.name)
         val lastNameEditText = findViewById<TextView>(R.id.lastName)
+        val phoneEditText = findViewById<TextView>(R.id.phone)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
         val registerButton = findViewById<Button>(R.id.registerButton)
+
 
         nameEditText.text = this.name
         lastNameEditText.text = this.lastName
+        phoneEditText.text = this.phone
+
+        if(savedRadioButtonId != 1) {
+            radioGroup.check(savedRadioButtonId)
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            selectedRadioButton = findViewById(R.id.radioGroup)
+            val selectedText = selectedRadioButton.text.toString()
+            Toast.makeText(this, "Selected: $selectedText", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+        //------Registerbutton below-------------------
 
         registerButton.setOnClickListener {
 
             this.name = nameEditText.text.toString()
             this.lastName = lastNameEditText.text.toString()
+            this.phone = phoneEditText.text.toString()
 
-            with(sharedPreferences.edit()) {
+            if (this.name.isEmpty() || this.lastName.isEmpty() || this.phone.isEmpty() || radioGroup.checkedRadioButtonId == -1) {
+                Toast.makeText(this, "Please fill in all fields and select a radio button", Toast.LENGTH_SHORT).show()
 
-                putString("name", this@MainActivity2.name)
-                putString("lastName", this@MainActivity2.lastName)
+            } else {
+                with(sharedPreferences.edit()) {
+
+                    putString("name", this@MainActivity2.name)
+                    putString("lastName", this@MainActivity2.lastName)
+                    putString("phone", this@MainActivity2.phone)
+                    putInt("selectedRadioButtonID", radioGroup.checkedRadioButtonId)
+                    apply()
+                }
+                write()
+
             }
-            write()
         }
-
-
-
-
 
         //-------------------------------------Bottom-------------------
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
@@ -79,7 +117,8 @@ class MainActivity2 : AppCompatActivity() {
         // Create a new user with a first and last name
         val user = hashMapOf(
             "name" to this.name,
-            "last" to this.lastName
+            "last" to this.lastName,
+            "phone" to this.phone
 
         )
         db.collection("users")
@@ -99,6 +138,9 @@ class MainActivity2 : AppCompatActivity() {
 
         outState.putString("name", this.name)
         outState.putString("lastName", this.lastName)
+        outState.putString("phone", this.phone)
+        outState.putInt("selectedRadioButton", findViewById<RadioGroup>(R.id.radioGroup).checkedRadioButtonId)
+
     }
 
 
